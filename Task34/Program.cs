29537fee8490945fee6f0b99856227ecf21ec3f6
Valeentin87 <Valeentin87@list.ru>
2022-08-25 15:04:01 +0300

    написали метод для печати массива

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 Console.WriteLine("Hello, World!");
+int[] array = new int[4];
+int[]
+PrintArray(array);
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+       Console.WriteLine($"{arr[i]} "); 
+    }
+}
