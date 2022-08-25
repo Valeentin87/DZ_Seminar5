e7fc8efe7a7d96dae9515da273b15ed1ca482215
Valeentin87 <Valeentin87@list.ru>
2022-08-25 15:11:28 +0300

    создали случайный трёхзначный массив

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,8 +1,17 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 Console.WriteLine("Hello, World!");
 int[] array = new int[4];
-int[]
-PrintArray(array);
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100,1000);
+    }
+    return arr;
+}
+int[] result = FillArray(array);
+PrintArray(result);
 void PrintArray(int[] arr)
 {
     for(int i = 0; i < arr.Length; i++)
