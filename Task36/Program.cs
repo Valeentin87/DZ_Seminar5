5b06f35973e203b8a5cf7b6d255a7e70d5061a3b
Valeentin87 <Valeentin87@list.ru>
2022-08-26 10:48:13 +0300

    Успешное решение задачи 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[4];
+int[] randomArray = FillArray(array);
+PrintArray(randomArray);
+Console.WriteLine();
+int result = SumOddPozition(randomArray);
+Console.WriteLine($"сумма нечётных элементов массива равна {result}");
+int SumOddPozition (int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i+=2)
+    {
+        sum+=array[i];
+    }
+    return sum;
+}
+
+
+
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next();
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
