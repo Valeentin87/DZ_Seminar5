7adc0142ef92cc6fe5b6f433903ec8396891b19e
Valeentin87 <Valeentin87@list.ru>
2022-08-26 09:35:48 +0300

    Успешное решение задачи 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,21 +1,35 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-Console.WriteLine("Hello, World!");
 int[] array = new int[4];
+int[] result = FillArray(array);
+PrintArray(result);
+Console.WriteLine();
+int numbEv = NumberEven(result);
+Console.WriteLine($"Количество положительных чётных чисел в массиве равно {numbEv}");
 int[] FillArray(int[] arr)
 {
     Random random = new Random();
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = random.Next(100,1000);
+        arr[i] = random.Next(100, 1000);
     }
     return arr;
 }
-int[] result = FillArray(array);
-PrintArray(result);
 void PrintArray(int[] arr)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-       Console.WriteLine($"{arr[i]} "); 
+        Console.Write($"{arr[i]} ");
     }
+}
+int NumberEven(int[] randomArray)
+{
+    int count = 0;
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        if (randomArray[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
 }
